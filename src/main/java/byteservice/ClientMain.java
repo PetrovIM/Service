848aa96fd303a991fecf7a5d27d347f4452943e5
Ilya Petrv <petrov_im@rambler.ru>
2023-09-14 10:08:56 +0300

    Доработка в методе close();

--- src/main/java/byteservice/ClientMain.java
+++ src/main/java/byteservice/ClientMain.java
@@ -6,23 +6,28 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         try (Client client = new Client("localhost",80)) {
             client.writeRequest("111+22 ");
             client.readResponse();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
-}
+    }
 
     static class Client implements AutoCloseable{
         private Socket clientSocket;
+        OutputStream out;
+        InputStream in;
 
         public Client(String ip, int port) throws IOException {
             this.clientSocket = new Socket(ip,port);
+            out = clientSocket.getOutputStream();
+            in = clientSocket.getInputStream();
             System.out.println("Клиент запустился");
         }
 
         public void writeRequest(String request) throws IOException {
-            OutputStream out = clientSocket.getOutputStream();
             byte[] q = request.getBytes();
             out.write(q);
             out.flush();
@@ -31,7 +36,6 @@ public class ClientMain {
 
         }
         public void readResponse() throws IOException {
-            InputStream in = clientSocket.getInputStream();
             int z;
             String str = "";
             while ((z = in.read()) != -1) {
@@ -44,9 +48,12 @@ public class ClientMain {
             System.out.println("Клиент закрылся");
         }
 
+
         @Override
-        public void close(){}
-
-
+        public void close() throws Exception {
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
     }
 }
